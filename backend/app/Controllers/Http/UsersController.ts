363b66770a3c1eb8c71c38b99b2a8dcf62eed822
Kevin Cabauatan <khevhinne@gmail.com>
2024-06-25 16:01:46 +0800

    fixed errors

--- backend/app/Controllers/Http/UsersController.ts
+++ backend/app/Controllers/Http/UsersController.ts
@@ -1,4 +1,4 @@
-//import UserRegisterValidator from 'App/Validators/UserRegisterValidator';
+import UserRegisterValidator from 'App/Validators/UserRegisterValidator';
 import UserUpdateValidator from 'App/Validators/UserUpdateValidator';
 import { User } from 'Database/entities/user';
 import { ic } from 'azle';
